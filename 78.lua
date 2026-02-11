1f1df163b9a2180e228087a348abac9123b49c09
xuaoxi9008 <3574769415@qq.com>
2026-02-11 11:04:58 +0800

    Add files via upload

---
+++ 78.lua
@@ -0,0 +1,20 @@
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/-v91/refs/heads/main/%E5%8A%A0%E8%BD%BD%E5%99%A8%E3%80%82.lua"))()
+local Window = WindUI:CreateWindow({
+    Title = "小西脚本<font color='#00FF00'>V2</font>",
+    Icon = "rbxassetid://4483362748",
+    IconTransparency = 0.5,
+    IconThemed = true,
+    Author = "作者:小西",
+    Folder = "CloudHub",
+    Size = UDim2.fromOffset(100, 100),
+    User = {
+        Enabled = true,
+        Callback = function() end,
+        Anonymous = false
+    },
+    KeySystem = {
+        Key = { "小西nb", "25ytgcjNB" },
+        Note = "卡密：小西nb",
+        SaveKey = false,
+    },
+})

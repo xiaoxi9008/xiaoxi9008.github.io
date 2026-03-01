4b515aceee8ab5d95078130be233b9069f492b4e
xuaoxi9008 <3574769415@qq.com>
2026-03-01 23:33:49 +0800

    Add files via upload

--- 服务器id加载.lua
+++ 服务器id加载.lua
@@ -1,6 +1,9 @@
 if game.PlaceId == 18687417158 then --- 服务器id
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Xiaoxi/refs/heads/main/NOL-%E4%BB%98%E8%B4%B9%E7%89%88%E6%9C%80%E6%96%B0%E6%BA%90%E7%A0%81.lua"))()---该服务器脚本
 
+ elseif game.PlaceId == 7239319209 then
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/Xiaoxi/refs/heads/main/obfuscated_script-1770370829905.lua"))() -- Ohio 服务器脚本
+ 
 else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xiaoxi9008/XIAOXIBUXINB/refs/heads/main/%E5%B0%8F%E8%A5%BF.lua"))()---加载通用raw
 
